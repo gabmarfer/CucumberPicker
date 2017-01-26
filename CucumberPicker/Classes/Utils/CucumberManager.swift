0be36cc398016962508dcdf98b415100d84d913b
gabmarfer <gabriel.martelo@gmail.com>
2017-01-26 12:21:41 +0100

    Add ImagePickerController with custom overlay view

---
+++ CucumberPicker/Classes/Utils/CucumberManager.swift
@@ -0,0 +1,17 @@
+//
+//  CucumberManager.swift
+//  CucumberPicker
+//
+//  Created by gabmarfer on 26/01/2017.
+//  Copyright © 2017 Kenoca Software. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+open class CucumberManager: NSObject, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
+    
+    
+}
---
+++ CucumberPicker/Classes/Views/HomeCell.swift
@@ -0,0 +1,14 @@
+//
+//  HomeCell.swift
+//  CucumberPicker
+//
+//  Created by gabmarfer on 24/01/2017.
+//  Copyright © 2017 Kenoca Software. All rights reserved.
+//
+
+import UIKit
+
+class HomeCell: UICollectionViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    
+}
